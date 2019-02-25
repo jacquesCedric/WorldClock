916261bd881cf436ae157fec3e25bea3c06382d1
Gold <jacobgaffney@me.com>
2019-02-25 23:08:02 +1100

    Initial commit

---
+++ WorldClock/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WorldClock
+//
+//  Created by Jacob Gold on 25/2/19.
+//  Copyright © 2019 Jacob Gold. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+    
+}
+
---
+++ WorldClock/ClockMenuController.swift
@@ -0,0 +1,24 @@
+//
+//  ClockMenuController.swift
+//  WorldClock
+//
+//  Created by Jacob Gold on 25/2/19.
+//  Copyright © 2019 Jacob Gold. All rights reserved.
+//
+
+import Cocoa
+
+class ClockMenuController: NSObject {
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+    override func awakeFromNib() {
+        statusItem.button?.title = "WorldClock"
+        statusItem.menu = statusMenu
+    }
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
+    }
+}
